82b67c4d849dcd7983c469cd4e834439124f4c21
Hashim <ZJAC057@live.rhul.ac.uk>
2023-11-11 13:17:52 +0000

    Added charts to display data on the homepage and moved gui.py methods to nn.py

--- pages/1_Nearest_Neighbours.py
+++ pages/1_Nearest_Neighbours.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -5,26 +6,26 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import GridSearchCV
-import gui
 
 label_encoder = LabelEncoder()
 
 def main():
+    st.title("Nearest Neighbours Page")
     data = load_data('fake_dataset.csv')
     X, y = preprocess_data(data)
     X_train, X_test, y_train, y_test = split_data(X, y)
     knn_model = train_knn_model(X_train, y_train)
 
-    hotel_name, star_rating, distance = gui.display_new_hotel_fields()
+    hotel_name, star_rating, distance = display_new_hotel_fields()
     
     new_price = predict_price(knn_model, hotel_name, star_rating, distance)
-    gui.display_prediction(new_price)
+    display_prediction(new_price)
 
     num_neighbours = list(range(1, 31))
     mse_values = calculate_mse_values(num_neighbours, X_train, X_test, y_train, y_test)
-    gui.display_mse_chart(num_neighbours, mse_values)
+    display_mse_chart(num_neighbours, mse_values)
     best_k, best_mse = find_best_k(X, y)
-    gui.display_best_k_and_mse(best_k, best_mse)
+    display_best_k_and_mse(best_k, best_mse)
 
 def load_data(file_path):
     data = pd.read_csv(file_path)
@@ -32,10 +33,8 @@ def load_data(file_path):
 
 def preprocess_data(data):
     data['Hotel Name'] = label_encoder.fit_transform(data['Hotel Name'])
-
     X = data.drop('Price', axis=1)
     y = data['Price']
-
     return X, y
 
 def split_data(X, y, test_size=0.2, random_state=42):
@@ -71,6 +70,26 @@ def calculate_mse_values(num_neighbours, X_train, X_test, y_train, y_test):
         y_pred = knn_model.predict(X_test)
         mse_values.append(calculate_mse(y_test, y_pred))
     return mse_values
+
+def display_new_hotel_fields():
+    st.subheader('Predict Hotel Price')
+    col1, col2, col3 = st.columns([3,1,1])
+    new_hotel_name = col1.text_input('Hotel Name', 'Hazel Inn')
+    star_rating = col2.number_input('Star Rating', 1, 5, 2)
+    distance = col3.number_input('Distance', 100, 5000, 100)
+
+    return new_hotel_name, star_rating, distance
+
+def display_prediction(result):
+    st.write(f'Predicted Price Per Night: £{result[0]:.2f}')
+
+def display_mse_chart(num_neighbours, mse_values):
+    st.subheader('Mean Squared Error for n_neighbors 1 to 25')
+    st.line_chart(dict(zip(num_neighbours, mse_values)))
+
+def display_best_k_and_mse(best_k, best_mse):
+    st.write(f'Best k: {best_k}')
+    st.write(f'Best Mean Squared Error: {best_mse:.2f}')
 
 if __name__ == '__main__':
     main()
