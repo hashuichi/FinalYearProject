299921f41cb7ae1eae5afde09a21bf806c9c84af
Hashim <ZJAC057@live.rhul.ac.uk>
2023-11-18 01:01:40 +0000

    Renamed dataset columns and reordered them

--- Home.py
+++ Home.py
@@ -3,20 +3,25 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import process_data
 
 def main():
     st.set_page_config(page_title="Financial Agent", layout="wide")
     st.title("Data")
-
-    data = pd.read_csv('fake_structured_data.csv')
-
+    data = get_data_selection()
     col1, col2 = st.columns([2, 1.5])
     col1.pyplot(display_price_distance_chart(data))
     col1.pyplot(display_price_star_rating_chart(data))
-    col2.table(renameDataColumns(data))
+    col2.table(rename_data_columns(data))
 
-def renameDataColumns(data):
+def get_data_selection():
+    data_radio = st.radio("**Select fake dataset:**", ["Structured Dataset", "Unstructured Dataset"])
+    if data_radio == 'Structured Dataset':
+        data = pd.read_csv('fake_structured_data.csv')
+    elif data_radio == 'Unstructured Dataset':
+        data = pd.read_csv('fake_data.csv')
+    return data
+
+def rename_data_columns(data):
     new_column_names = {
         'star_rating': 'Star Rating (1-5)',
         'distance': 'Distance to city centre (Metres)',
