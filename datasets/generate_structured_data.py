4ce503fccfe0c89ee10571aa0d5cfb3e5703f91d
Hashim <ZJAC057@live.rhul.ac.uk>
2023-11-22 14:39:48 +0000

    removed redundant comments

--- datasets/generate_structured_data.py
+++ datasets/generate_structured_data.py
@@ -1,10 +1,7 @@
 import pandas as pd
 import numpy as np
 
-# Set random seed for reproducibility
 np.random.seed(0)
-
-# Number of hotels
 num_hotels = 100
 
 hotel_names = [f"Hotel {i}" for i in range(1, num_hotels + 1)]
@@ -18,24 +15,17 @@ distances = np.random.randint(100, 5001, num_hotels)
 distances_normalized = (distances - distances.min()) / (distances.max() - distances.min())
 
 price_noise = np.random.randint(-20, 21, num_hotels)  # Adjust the range as needed
-
-# Calculate prices based on a combination of star ratings and normalized distances
-# You can adjust the coefficients as needed
 prices = 50 + star_ratings * 50 - distances_normalized * 200 + price_noise
 
-# Adjust prices to fall within the range of 50-500
 min_price = 50
 max_price = 500
 
-# Calculate the min and max values of the calculated prices
 min_calculated_price = min(prices)
 max_calculated_price = max(prices)
 
-# Scale prices to fit the desired range
 prices = ((prices - min_calculated_price) / (max_calculated_price - min_calculated_price)) * (max_price - min_price) + min_price
 prices = np.round(prices).astype(int)
 
-# Create a DataFrame
 data = {
     "star_rating": star_ratings,
     "distance": distances,
@@ -43,9 +33,5 @@ data = {
 }
 
 df = pd.DataFrame(data)
-
-# Print the DataFrame
 print(df)
-
-# Save the DataFrame to a CSV file
 df.to_csv('fake_structured_data.csv', index=False)
