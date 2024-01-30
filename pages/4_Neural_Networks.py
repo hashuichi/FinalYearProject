2409f33d2ec5b6f946957dc7ca73db785e3287c3
Hashim <ZJAC057@live.rhul.ac.uk>
2024-01-30 12:06:26 +0000

    preprocessing and testing dataset

--- pages/4_Neural_Networks.py
+++ pages/4_Neural_Networks.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 class NeuralNetworksPage:
     def __init__(self):
@@ -6,7 +7,18 @@ class NeuralNetworksPage:
 
     def run(self):
         st.title("Neural Networks")
-        st.write("Neural Networks implementation will be displayed here...")
+
+        df = pd.read_csv('datasets/airbnb_london.csv')
+        # df['property_type'] = df['property_type'].apply(lambda x: 'Private Room' if 'private room' in x.lower() else x)
+        # df['property_type'] = df['property_type'].apply(lambda x: 'Shared Room' if 'shared room' in x.lower() else x)
+        # df['property_type'] = df['property_type'].apply(lambda x: x.replace('Entire', '').strip().capitalize() if 'Entire' in x else x)
+        property_types = df['room_type'].unique()
+        selected_property_type = st.selectbox('Select Room Type', property_types)
+        # Filter DataFrame based on selected property type
+        filtered_data = df[df['room_type'] == selected_property_type]
+        filtered_data_reset_index = filtered_data.reset_index(drop=True)
+        st.dataframe(filtered_data_reset_index)
+
 
 if __name__ == '__main__':
     app = NeuralNetworksPage()
