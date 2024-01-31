c99f1f13267fe7be3eb1e7b9e2ae6c510a9f9252
Hashim <ZJAC057@live.rhul.ac.uk>
2024-01-31 18:13:54 +0000

    deleted empty data entries

--- pages/4_Neural_Networks.py
+++ pages/4_Neural_Networks.py
@@ -9,15 +9,13 @@ class NeuralNetworksPage:
         st.title("Neural Networks")
 
         df = pd.read_csv('datasets/airbnb_london.csv')
-        # df['property_type'] = df['property_type'].apply(lambda x: 'Private Room' if 'private room' in x.lower() else x)
-        # df['property_type'] = df['property_type'].apply(lambda x: 'Shared Room' if 'shared room' in x.lower() else x)
-        # df['property_type'] = df['property_type'].apply(lambda x: x.replace('Entire', '').strip().capitalize() if 'Entire' in x else x)
-        property_types = df['room_type'].unique()
-        selected_property_type = st.selectbox('Select Room Type', property_types)
+
+        selected_room_type = st.selectbox('Select Room Type', df['room_type'].unique())
+
         # Filter DataFrame based on selected property type
-        filtered_data = df[df['room_type'] == selected_property_type]
-        filtered_data_reset_index = filtered_data.reset_index(drop=True)
-        st.dataframe(filtered_data_reset_index)
+        filtered_data = df[df['room_type'] == selected_room_type]
+        filtered_data = filtered_data.reset_index(drop=True)
+        st.dataframe(filtered_data)
 
 
 if __name__ == '__main__':
