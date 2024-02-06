a51cb981e87eac5fe8349123b0fd2094df9f36cb
Hashim <ZJAC057@live.rhul.ac.uk>
2024-02-06 13:39:58 +0000

    comments

--- pages/4_Neural_Networks.py
+++ pages/4_Neural_Networks.py
@@ -8,14 +8,14 @@ class NeuralNetworksPage:
     def run(self):
         st.title("Neural Networks")
 
-        df = pd.read_csv('datasets/airbnb_london.csv')
+        # df = pd.read_csv('datasets/airbnb_london.csv')
 
-        selected_room_type = st.selectbox('Select Room Type', df['room_type'].unique())
+        # selected_room_type = st.selectbox('Select Room Type', df['room_type'].unique())
 
-        # Filter DataFrame based on selected property type
-        filtered_data = df[df['room_type'] == selected_room_type]
-        filtered_data = filtered_data.reset_index(drop=True)
-        st.dataframe(filtered_data)
+        # # Filter DataFrame based on selected property type
+        # filtered_data = df[df['room_type'] == selected_room_type]
+        # filtered_data = filtered_data.reset_index(drop=True)
+        # st.dataframe(filtered_data)
 
 
 if __name__ == '__main__':
