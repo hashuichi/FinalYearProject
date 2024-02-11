dfcf1f2804aeee7e72a4ddb1df497657eee25fe4
Hashim <ZJAC057@live.rhul.ac.uk>
2024-02-11 19:12:10 +0000

    generated plots for benchmark dataset

--- Data.py
+++ Data.py
@@ -7,15 +7,28 @@ from data_loader import DataLoader
 class DataPage:
     def __init__(self):
         self.data = None
+        self.room_type_map = {
+            1: 'Private room',
+            2: 'Entire home/apt',
+            3: 'Shared room',
+            4: 'Hotel room'
+        }
 
     def run(self):
         st.set_page_config(page_title="Data", layout="wide")
         self.load_data()
         st.title("Data")
-        col1, col2 = st.columns([2, 1.5])
-        col1.pyplot(self.display_price_distance_chart())
-        col1.pyplot(self.display_price_star_rating_chart())
-        col2.table(self.rename_data_columns())
+        col1, col2 = st.columns([2, 1])
+        if st.session_state.selected_df == "Benchmark Dataset":
+            with col1:
+                self.generate_plots()
+            col2.write(self.rename_benchmark_data())
+            col2.subheader("Room Type Key")
+            col2.table(self.room_type_map)
+        else:
+            col1.pyplot(self.display_price_distance_chart())
+            col1.pyplot(self.display_price_star_rating_chart())
+            col2.table(self.rename_fake_data())
 
     def load_data(self):
         data_option = self.get_data_selection()
@@ -26,9 +39,23 @@ class DataPage:
             st.session_state.df = self.data
 
     def get_data_selection(self):
-        return st.selectbox("**Select Dataset**", ["Benchmark Dataset", "Structured Dataset", "Unstructured Dataset"], key='selected_df')
+        return st.selectbox("**Select Dataset**", ["Choose a Dataset", "Benchmark Dataset", "Structured Dataset", "Unstructured Dataset"], key='selected_df')
 
-    def rename_data_columns(self):
+    def rename_benchmark_data(self):
+        if self.data is not None:
+            new_column_names = {
+                'room_type': 'Room Type',
+                'accommodates': 'Occupants',
+                'bathrooms': 'Bathrooms',
+                'beds': 'Beds',
+                'price': 'Price ($)'
+            }
+            return self.data.rename(columns=new_column_names)
+        else:
+            st.warning("No data selected.")
+            return pd.DataFrame()
+        
+    def rename_fake_data(self):
         if self.data is not None:
             new_column_names = {
                 'star_rating': 'Star Rating (1-5)',
@@ -42,12 +69,20 @@ class DataPage:
 
     def display_price_distance_chart(self):
         if self.data is not None:
-            fig, ax = plt.subplots()
-            scatter = ax.scatter(self.data['distance'], self.data['price'], c=self.data['star_rating'], cmap='viridis')
-            ax.set_xlabel('Distance')
-            ax.set_ylabel('Price')
-            ax.legend(*scatter.legend_elements(), title='Star Rating')
-            return fig
+            if st.session_state.selected_df == "Benchmark Dataset":
+                fig, ax = plt.subplots()
+                scatter = ax.scatter(self.data['distance'], self.data['price'], c=self.data['star_rating'], cmap='viridis')
+                ax.set_xlabel('Distance')
+                ax.set_ylabel('Price')
+                ax.legend(*scatter.legend_elements(), title='Star Rating')
+                return fig
+            else:
+                fig, ax = plt.subplots()
+                scatter = ax.scatter(self.data['distance'], self.data['price'], c=self.data['star_rating'], cmap='viridis')
+                ax.set_xlabel('Distance')
+                ax.set_ylabel('Price')
+                ax.legend(*scatter.legend_elements(), title='Star Rating')
+                return fig
         else:
             st.warning("No data selected.")
             return plt.figure()
@@ -63,6 +98,49 @@ class DataPage:
         else:
             st.warning("No data selected.")
             return plt.figure()
+        
+    def generate_plots(self):
+        col1, col2 = st.columns(2)
+        
+        # Histograms
+        room_type_counts = self.data['room_type'].map(self.room_type_map).value_counts()
+        fig, ax = plt.subplots()
+        room_type_counts.plot(kind='bar', ax=ax)
+        ax.set_xlabel('Room Type')
+        ax.set_ylabel('Frequency')
+        ax.set_xticklabels(room_type_counts.index, rotation=45)
+        col1.pyplot(fig)
+        for col in self.data.select_dtypes(include='number').columns.drop(['room_type', 'price']):
+            fig, ax = plt.subplots()
+            ax.hist(self.data[col])
+            ax.set_xlabel(col)
+            ax.set_ylabel('Frequency')
+            col1.pyplot(fig)
+
+        # Scatter plot for price vs accommodates
+        fig, ax = plt.subplots()
+        ax.scatter(self.data['accommodates'], self.data['price'])
+        ax.set_xlabel('Accommodates')
+        ax.set_ylabel('Price')
+        ax.grid(True)
+        col2.pyplot(fig)
+
+        # Scatter plot for price vs beds
+        fig, ax = plt.subplots()
+        ax.scatter(self.data['beds'], self.data['price'])
+        ax.set_xlabel('Beds')
+        ax.set_ylabel('Price')
+        ax.grid(True)
+        col2.pyplot(fig)
+
+        # Box plot for price based on room type
+        fig, ax = plt.subplots(figsize=(8, 6))
+        self.data.boxplot(column='price', by='room_type', ax=ax)
+        ax.set_ylabel('Price')
+        ax.set_xlabel('Room Type')
+        ax.set_xticklabels(['Private room', 'Entire home/apt', 'Shared room', 'Hotel room'])
+        ax.grid(True)
+        col2.pyplot(fig)
 
 if __name__ == '__main__':
     app = DataPage()
